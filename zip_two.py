a058845105a2fcc05d2a52e4ea70eb0d33a5cbc8
ysl4567 <117042067+ysl4567@users.noreply.github.com>
2022-11-25 23:12:00 -0800

    Add files via upload

---
+++ zip_two.py
@@ -0,0 +1,37 @@
+import math
+#zip
+a = ("John", "Charles", "Mike")
+b = ("Jenny", "Christy", "Monica")
+x = zip(a, b)
+print (list(x))
+
+print ("\n")
+
+#iter,zip
+y = iter(zip(a,b))
+print (next(y))
+print (next(y))
+print (next(y))
+print (len(y))
+
+
+
+print ("\n")
+
+
+city = ["asdf","sdfg","dfgh"]
+place = ['234','645','9876']
+combination = zip(city,place)
+print (list(combination))
+
+print ("\n")
+
+
+
+
+
+
+
+
+
+
