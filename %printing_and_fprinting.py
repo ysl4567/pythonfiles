716ef48dc5ff783ef4426b4ec5bd239723144137
ysl4567 <117042067+ysl4567@users.noreply.github.com>
2022-11-26 22:11:14 -0800

    Add files via upload

---
+++ %printing_and_fprinting.py
@@ -0,0 +1,42 @@
+
+name = "Jose"
+number = 42
+print ("%s is %d years old" % (name,number))
+print (f"{name} I am {number} years old")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+a_value = "asdf"
+b_value = 234
+
+print (f"Hello, my name is {a_value}. I am {b_value} years old")
+print ("Hello, my name is %s. I am %d years old " % (a_value, b_value))
