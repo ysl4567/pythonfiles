904c5a8ed409c29617ae46b6a47aafc12239497b
ysl4567 <117042067+ysl4567@users.noreply.github.com>
2022-12-16 19:09:38 -0800

    Create f_string.py

---
+++ f_string.py
@@ -0,0 +1,22 @@
+name = "Anthony"
+
+print ("My name is", name)
+print (f"My name is {name}")
+
+
+mydict = {'name':'Anthony'}
+print(f'My name is {mydict["name"]}')
+
+
+mylist = ['Anthony', 'Bourdain']
+print(f'My name is {mylist[0]}')
+print ("My name is",mylist[0])
+
+
+
+age = 34
+name = "Tom"
+
+print (f"Hello my name is {name}. I am {age}")
+
+print ("Hello my name is %s. I am %d years old" % (name,age))
